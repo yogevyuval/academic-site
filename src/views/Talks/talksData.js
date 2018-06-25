4e42528a982f2ec200d8704bcc208b0f518a6ff2
eylonyogev <40497160+eylonyogev@users.noreply.github.com>
2018-06-25 23:20:35 +0300

    removed links

--- src/views/Talks/talksData.js
+++ src/views/Talks/talksData.js
@@ -1,25 +1,21 @@
 const talksData = [
   {
     title: "Tight Bounds for Sliding Bloom Filters",
-    link: "",
     date: "October 2013",
     location: "Theory Lunch, Weizmann Institute" 
   },
   {
     title: "Tight Bounds for Sliding Bloom Filters",
-    link: "",
     date: "2013",
     location: "ISSAC Conference, Hong Kong University" 
   },
   {
     title: "Can We obfuscate Programs?",
-    link: "",
     date: "July 2014",
     location: "Theory Lunch, Weizmann Institute" 
   },
   {
     title: "Secret Sharing for NP.",
-    link: "",
     date: "December 2014",
     location: "GTACS, MTA" 
   },
@@ -31,73 +27,61 @@ const talksData = [
   },
   {
     title: "Functional Encryption for Randomized Functionalities in the Private-Key Setting from Minimal Assumptions",
-    link: "",
     date: "March 2015",
     location: "TCC Conference, Warsaw" 
   },
   {
     title: "Bloom Filters in Adversarial Environments",
-    link: "",
     date: "June 2015",
     location: "Theory Lunch, Weizmann Institute" 
   },
   {
     title: "Bloom Filters in Adversarial Environments",
-    link: "",
     date: "August 2015",
     location: "Simons Institute, Cryptography Semester" 
   },
   {
     title: "Bloom Filters in Adversarial Environments",
-    link: "",
     date: "August 2015",
     location: "CRYPTO Conference, Santa Barbara" 
   },
   {
     title: "Can We Obfuscate Programs?",
-    link: "",
     date: "September 2015",
     location: "Machine Learning Seminar, Microsoft" 
   },
   {
     title: "Bloom Filters in Adversarial Environments",
-    link: "",
     date: "October 2015",
     location: "Theory Lunch, Technion" 
   },
   {
     title: "The Journey from NP to TFNP Hardness",
-    link: "",
     date: "January 2017",
     location: "Theory Lunch, Weizmann Institute" 
   },
   {
     title: "Distributed Computing Made Secure: A New Cycle Cover Theorem",
-    link: "",
     date: "January 2017",
     location: "Theory Seminar, Weizmann Institute" 
   },
   {
     title: "Bloom Filters in Adversarial Environments",
-    link: "",
     date: "March 2017",
     location: "Ariel University" 
   },
   {
     title: "Bloom Filters in Adversarial Environments",
-    link: "",
     date: "May 2017",
     location: "Ben Gurion University" 
   },
   {
     title: "Hardness of Continuous Local Search: Query Complexity and Cryptographic Lower Bounds",
-    link: "",
     date: "May 2017",
     location: "Advanced Algorithms Seminar, Tel Aviv University" 
   },
   {
     title: "Collision Resistant Hashing for Paranoids: Dealing with Multiple Collisions",
-    link: "",
     date: "June 2017",
     location: "GTACS, MTA" 
   },
@@ -109,61 +93,51 @@ const talksData = [
   },
   {
     title: "Hardness of Continuous Local Search: Query Complexity and Cryptographic Lower Bounds",
-    link: "",
     date: "January 2017",
     location: "Theory Lunch, Berkeley" 
   },
   {
     title: "The Journey from NP to TFNP Hardness",
-    link: "",
     date: "January 2017",
     location: "Theory Lunch, Stanford" 
   },
   {
     title: "Search Problems: A Cryptographic Perspective",
-    link: "",
     date: "July 2017",
     location: "China Theory Week, Shanghai" 
   },
   {
     title: "White-Box vs. Black-Box Complexity of Search Problems: Ramsey and Graph Property Testing",
-    link: "",
     date: "October 2017",
     location: "FOCS Conference" 
   },
   {
     title: "Collision Resistant Hashing for Paranoids: Dealing with Multiple Collisions",
-    link: "",
     date: "November 2017",
     location: "Theory Seminar, Bar Ilan University" 
   },
   {
     title: "Another Step Towards Realizing Random Oracles: Non-Malleable Point Obfuscation",
-    link: "",
     date: "April 2018",
     location: "Eurocrypt Conference, Tel Aviv" 
   },
   {
     title: "Collision Resistant Hashing for Paranoids: Dealing with Multiple Collisions",
-    link: "",
     date: "April 2018",
     location: "Eurocrypt Conference, Tel Aviv" 
   },
   {
     title: "Distributed Computing Made Secure: A New Cycle Cover Theorem",
-    link: "",
     date: "April 2018",
     location: "3rd CryptoAction Symposium, Montenegro" 
   },
   {
     title: "Search Problems: A Cryptographic Perspective",
-    link: "",
     date: "June 2018",
     location: "Festive PhD Lecture, Weizmmann" 
   },
   {
     title: "On Distributional Collision Resistant Hash",
-    link: "",
     date: "June 2018",
     location: "TheorySeminar, Technion" 
   }
