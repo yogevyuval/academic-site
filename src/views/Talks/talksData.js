dd2680780c819c4e83dceb0c22dd2f385a53ccde
eylonyogev <40497160+eylonyogev@users.noreply.github.com>
2018-06-25 22:58:43 +0300

    some talks

--- src/views/Talks/talksData.js
+++ src/views/Talks/talksData.js
@@ -1,6 +1,87 @@
 const talksData = [
   {
-
+    title: "Tight Bounds for Sliding Bloom Filters",
+    link: "",
+    date: "October 2013",
+    location: "Theory Lunch, Weizmann Institute" 
+  },
+  {
+    title: "Tight Bounds for Sliding Bloom Filters",
+    link: "",
+    date: "2013",
+    location: "ISSAC Conference, Hong Kong University" 
+  },
+  {
+    title: "Can We obfuscate Programs?",
+    link: "",
+    date: "July 2014",
+    location: "Theory Lunch, Weizmann Institute" 
+  },
+  {
+    title: "",
+    link: "",
+    date: "",
+    location: "" 
+  },
+  {
+    title: "",
+    link: "",
+    date: "",
+    location: "" 
+  },
+  {
+    title: "",
+    link: "",
+    date: "",
+    location: "" 
+  },
+  {
+    title: "",
+    link: "",
+    date: "",
+    location: "" 
+  },
+  {
+    title: "",
+    link: "",
+    date: "",
+    location: "" 
+  },
+  {
+    title: "",
+    link: "",
+    date: "",
+    location: "" 
+  },
+  {
+    title: "",
+    link: "",
+    date: "",
+    location: "" 
+  },
+  {
+    title: "",
+    link: "",
+    date: "",
+    location: "" 
+  },
+  {
+    title: "",
+    link: "",
+    date: "",
+    location: "" 
+  },
+  {
+    title: "",
+    link: "",
+    date: "",
+    location: "" 
+  },
+  {
+    title: "",
+    link: "",
+    date: "",
+    location: "" 
   }
 ];
 
